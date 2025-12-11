b20d665eddbd008dcc585a85663c8203304c8dfd
TheBOI175 <a58185704@gmail.com>
2025-12-10 18:23:21 -0800

    Refactor server.py to streamline room handling

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
-from flask import Flask, send_from_directory
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask
+from flask_socketio import SocketIO, emit, join_room, leave_room, request
 import random
-import string
 import os
 
-app = Flask(__name__, static_folder='public')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'rip-chat-secret-key'
 
 # Allow all origins for CORS (needed for standalone HTML files)
@@ -19,12 +18,6 @@ def generate_room_code():
     """Generate a random 6-character room code"""
     chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'
     return ''.join(random.choice(chars) for _ in range(6))
-
-
-# Serve the HTML file
-@app.route('/')
-def index():
-    return send_from_directory('public', 'index.html')
 
 
 @socketio.on('connect')
@@ -44,16 +37,11 @@ def handle_create_room(username):
     sid = request.sid
     room_code = generate_room_code()
     
-    # Create the room
     rooms[room_code] = {'users': {}}
     rooms[room_code]['users'][sid] = {'username': username, 'socketId': sid}
-    
-    # Track user data
     user_data[sid] = {'username': username, 'room_code': room_code}
     
-    # Join the socket.io room
     join_room(room_code)
-    
     emit('room-created', {'roomCode': room_code, 'username': username})
     print(f'Room {room_code} created by {username}')
 
@@ -64,29 +52,23 @@ def handle_join_room(data):
     room_code = data['roomCode'].upper()
     username = data['username']
     
-    # Check if room exists
     if room_code not in rooms:
         emit('error', 'Room not found. Check the code and try again.')
         return
     
-    # Get existing users before adding new one
     existing_users = list(rooms[room_code]['users'].values())
     
-    # Add user to room
     rooms[room_code]['users'][sid] = {'username': username, 'socketId': sid}
     user_data[sid] = {'username': username, 'room_code': room_code}
     
-    # Join the socket.io room
     join_room(room_code)
     
-    # Tell the new user about existing users
     emit('room-joined', {
         'roomCode': room_code,
         'username': username,
         'existingUsers': existing_users
     })
     
-    # Tell existing users about the new user
     emit('user-joined', {
         'username': username,
         'socketId': sid
@@ -99,11 +81,10 @@ def handle_join_room(data):
 def handle_offer(data):
     sid = request.sid
     target_id = data['targetId']
-    offer = data['offer']
     
     if sid in user_data:
         emit('offer', {
-            'offer': offer,
+            'offer': data['offer'],
             'fromId': sid,
             'fromUsername': user_data[sid]['username']
         }, to=target_id)
@@ -111,26 +92,18 @@ def handle_offer(data):
 
 @socketio.on('answer')
 def handle_answer(data):
-    sid = request.sid
-    target_id = data['targetId']
-    answer = data['answer']
-    
     emit('answer', {
-        'answer': answer,
-        'fromId': sid
-    }, to=target_id)
+        'answer': data['answer'],
+        'fromId': request.sid
+    }, to=data['targetId'])
 
 
 @socketio.on('ice-candidate')
 def handle_ice_candidate(data):
-    sid = request.sid
-    target_id = data['targetId']
-    candidate = data['candidate']
-    
     emit('ice-candidate', {
-        'candidate': candidate,
-        'fromId': sid
-    }, to=target_id)
+        'candidate': data['candidate'],
+        'fromId': request.sid
+    }, to=data['targetId'])
 
 
 @socketio.on('leave-room')
@@ -146,30 +119,22 @@ def handle_user_leave(sid):
     room_code = user_data[sid]['room_code']
     
     if room_code and room_code in rooms:
-        # Remove user from room
         if sid in rooms[room_code]['users']:
             del rooms[room_code]['users'][sid]
         
-        # Notify others
         emit('user-left', {
             'socketId': sid,
             'username': username
         }, to=room_code)
         
-        # Leave socket.io room
         leave_room(room_code)
         
-        # Delete empty rooms
         if len(rooms[room_code]['users']) == 0:
             del rooms[room_code]
             print(f'Room {room_code} deleted (empty)')
     
-    # Clean up user data
     del user_data[sid]
 
-
-# Need to import request for sid access
-from flask_socketio import request
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
